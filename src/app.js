d4057ca542c50399e579ed7cf6c5f885a0f66042
NicoleBari <nicole.barisich.l@gmail.com>
2022-06-25 16:45:11 +0000

    Random Card Generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let tipos = ["♥", "♠", "♣", "♦"];
+  let carta = Math.floor(Math.random() * 4);
+  document.querySelector(".up").innerHTML = tipos[carta];
+  document.querySelector(".down").innerHTML = tipos[carta];
+  if (carta == 0 || carta == 3) {
+    document.querySelector(".down").style.color = "red";
+    document.querySelector(".up").style.color = "red";
+  }
+
+  let num = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let n = Math.floor(Math.random() * 14);
+  document.querySelector("p").innerHTML = num[n];
 };
